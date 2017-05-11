0a63ebddefc1c44fca6d61bf560dc2212230ec43
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-05-11 11:38:48 -0700

    adding the basic infrastructure

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Coverage Status](https://coveralls.io/repos/github/biocore/genome-subsampler/badge.svg?branch=master)](https://coveralls.io/github/biocore/genome-subsampler?branch=master)
+[![Build Status](https://travis-ci.org/biocore/genome-subsampler.svg?branch=master)](https://travis-ci.org/biocore/genome-subsampler)
+
+# genome subsampler
+genome subsampler is coded in Python 3.x
+
---
+++ setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, genome-subsampler development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import re
+import ast
+from setuptools import find_packages, setup
+
+
+# version parsing from __init__ pulled from Flask's setup.py
+# https://github.com/mitsuhiko/flask/blob/master/setup.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('genome-subsampler/__init__.py', 'rb') as f:
+    hit = _version_re.search(f.read().decode('utf-8')).group(1)
+    version = str(ast.literal_eval(hit))
+
+classifiers = [
+    'Development Status :: 1 - Pre-Alpha',
+    'License :: OSI Approved :: BSD License',
+    'Environment :: Console',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Operating System :: Unix',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows']
+
+
+description = 'genome-subsampler: ?'
+with open('README.md') as f:
+    long_description = f.read()
+
+keywords = 'genome subsampler prototype selection',
+
+setup(name='genome-subsampler',
+      version=version,
+      license='BSD',
+      description=description,
+      long_description=long_description,
+      keywords=keywords,
+      classifiers=classifiers,
+      author="genome-subsampler development team",
+      author_email="sjanssen@ucsd.edu",
+      maintainer="genome-subsampler development team",
+      maintainer_email="sjanssen@ucsd.edu",
+      url='https://github.com/biocore/genome-subsampler',
+      test_suite='nose.collector',
+      packages=find_packages(),
+      install_requires=[
+          'click >= 6',
+          'scikit-bio >= 0.4.0',
+      ],
+      extras_require={'test': ["nose", "pep8", "flake8"],
+                      'coverage': ["coverage"]})
