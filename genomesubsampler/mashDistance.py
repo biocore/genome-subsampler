6b48cc2a1595945eabc286c8b5a69d99f7ebf9cb
priya <priya@kl-mac-FFT4.local>
2017-07-24 14:34:24 -0700

    WIP mash distance

--- genomesubsampler/mashDistance.py
+++ genomesubsampler/mashDistance.py
@@ -14,15 +14,17 @@
 import os
 import gzip
 from subprocess import call
+import click
 
-
-def compute_mash_distance(genome_dir, genome_ext='fna', cpus=1):
+def compute_mash_distance(genome_dir, mash_dist_fp, genome_ext='fna', cpus=1):
     """Compute pairwise MinHash distances between genomes in a given path.
 
     Parameters
     ----------
     genome_dir : str
         directory containing input genomes
+    mash_dist_fp : str
+        file path to output pairwise MinHash distance list
     genome_ext : str
         extension name of input genome files (default: fna)
     cpus : int
@@ -46,36 +48,40 @@ def compute_mash_distance(genome_dir, genome_ext='fna', cpus=1):
 
     # paste sketches in one file
     call('sed "s/$/.msh/" genomes.txt  > genome_mshs.txt', shell=True)
+    os.remove('genomes.txt')
     call('mash paste -l genomes.msh genome_mshs.txt', shell=True)
-    call('rm genome_mshs.txt', shell=True)
+    # call('rm genome_mshs.txt', shell=True)
+    os.remove('genome_mshs.txt')
 
     # compute pairwise distances based on sketches
-    call('mash dist -p %s genomes.msh genomes.msh | gzip > genomes.dist.gz'
-         % cpus, shell=True)
+    call('mash dist -p %s genomes.msh genomes.msh | gzip > %s'
+         % (cpus, mash_dist_fp), shell=True)
+    os.remove('genomes.msh')
 
 
-def make_distance_matrix(dist_dir):
-    """Compute distance matrix based on pairwise MinHash distances
+def make_distance_matrix(mash_dist_fp, dist_mat_fp):
+    """Compute distance matrix based on pairwise MinHash distances.
 
     Parameters
     ----------
-    dist_dir: str
-        path to directory with pairwise MinHash distances between genomes
+    mash_dist_fp : str
+        file path to input pairwise MinHash distance list
+    dist_mat_fp : str
+        file path to output MinHash distance matrix
     """
     ids = []
     vals = []
     reading_ids = True
-    fpath = os.path.join(dist_dir, 'genomes.dist.gz')
-    with open('dist_matrix.txt', 'w') as fh:
-        with gzip.open(fpath, 'r') as fdist:
+    with open(dist_mat_fp, 'w') as fh:
+        with gzip.open(mash_dist_fp, 'r') as fdist:
             for line in fdist:
-                line=line.decode("utf-8")
+                line = line.decode('utf-8')
                 source, target, distance = line.rstrip('\r\n').split('\t')[:3]
                 if reading_ids:
                     # read ids and values of the first id
                     if not ids and source != target:
                         raise ValueError('First source'
-                                         'and target do not match')
+                                         'and target do not match.')
                     if not ids or source != ids[0]:
                         ids.append(source)
                         vals.append(distance)
@@ -89,7 +95,7 @@ def make_distance_matrix(dist_dir):
                         isrc, itag, ctag, vals = 1, 1, target, [distance]
                         if target != ids[1]:
                             raise ValueError('Second source'
-                                             'and target do not match')
+                                             'and target do not match.')
                 else:
                     # read and write the rest of values
                     if target == ctag:
@@ -109,3 +115,28 @@ def make_distance_matrix(dist_dir):
                         fh.write('%s\t%s\n' % (ctag, '\t'.join(vals)))
                         isrc, ctag, vals = 1, target, [distance]
         fh.write('%s\t%s\n' % (ctag, '\t'.join(vals)))
+
+
+@click.command()
+# genome_dir, mash_dist_fp, genome_ext='fna', cpus=1
+@click.option('--genome-dir', '-d', required=True,
+              type=click.Path(resolve_path=True, readable=True, exists=True,
+                              dir_okay=True, file_okay=False),
+              help='Directory containing input genomes')
+@click.option('--genome_ext', '-e', required=False, type=str, default='fna',
+              help='Extension name of input genome files')
+@click.option('--cpus', '-p', required=False, type=int, default=1,
+              help='Number of CPU cores to use')
+@click.option('--m', '-d', required=True,
+              type=click.Path(resolve_path=True, readable=True, exists=True,
+                              dir_okay=True, file_okay=False),
+              help='Directory containing input genomes')
+def _main(repophlan_wscores_fp):
+    """Calculate MinHash distances for input genomes.
+    """
+    out = parse_repophlan(repophlan_wscores_fp)
+    click.echo('\n'.join(out))
+    click.echo('Task completed.')
+
+if __name__ == "__main__":
+    _main()
---
+++ genomesubsampler/tests/test_mashDistance.py
@@ -0,0 +1,62 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017--, genome-subsampler development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+from os.path import join
+from tempfile import mkdtemp
+from shutil import rmtree
+import gzip
+
+from skbio.util import get_data_path
+
+from genomesubsampler.mashDistance import (
+    compute_mash_distance,
+    make_distance_matrix)
+
+
+class MashDistanceTests(TestCase):
+    """Tests for mashDistance.py."""
+
+    def setUp(self):
+        """Create working directory and test files.
+        """
+        self.wkdir = mkdtemp()
+        self.genome_dir = get_data_path('test_fna')
+        self.out_mash_dist_fp = join(self.wkdir, 'out.dist.gz')
+        self.mash_dist_fp = get_data_path('genomes.dist.gz')
+        self.out_dist_mat_fp = join(self.wkdir, 'out_dist_mat.txt')
+        self.dist_mat_fp = get_data_path('dist_matrix.txt')
+
+    def tearDown(self):
+        """Delete working directory and test files.
+        """
+        rmtree(self.wkdir)
+
+    def test_compute_mash_distance(self):
+        """Test the output of function "compute_mash_distance".
+        """
+        compute_mash_distance(self.genome_dir, self.out_mash_dist_fp)
+        with gzip.open(self.out_mash_dist_fp, 'r') as fh:
+            obs = fh.read()
+        with gzip.open(self.mash_dist_fp, 'r') as fh:
+            exp = fh.read()
+        self.assertEqual(obs, exp)
+
+    def test_make_distance_matrix(self):
+        """Test the output of function "make_distance_matrix".
+        """
+        make_distance_matrix(self.mash_dist_fp, self.out_dist_mat_fp)
+        with open(self.out_dist_mat_fp, 'r') as fh:
+            obs = fh.read()
+        with open(self.dist_mat_fp, 'r') as fh:
+            exp = fh.read()
+        self.assertEqual(obs, exp)
+
+
+if __name__ == '__main__':
+    main()
